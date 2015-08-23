62838400284406a0c6a56d204a5989955d683e77
David Edgar <david.r.edgar@gmail.com>
2015-08-23 16:48:07 +0100

    Add config files for use on google cloud instance.

---
+++ admin/config-google_cloud.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://104.155.84.193/opencart/admin/');
+define('HTTP_CATALOG', 'http://104.155.84.193/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://104.155.84.193/opencart/admin/');
+define('HTTPS_CATALOG', 'https://104.155.84.193/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/admin/');
+define('DIR_SYSTEM', '/var/www/opencart/system/');
+define('DIR_LANGUAGE', '/var/www/opencart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/opencart/image/');
+define('DIR_CACHE', '/var/www/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/system/download/');
+define('DIR_UPLOAD', '/var/www/opencart/system/upload/');
+define('DIR_LOGS', '/var/www/opencart/system/logs/');
+define('DIR_MODIFICATION', '/var/www/opencart/system/modification/');
+define('DIR_CATALOG', '/var/www/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mtesitoo');
+define('DB_PREFIX', 'oc_');
---
+++ config-google_cloud.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://104.155.84.193/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://104.155.84.193/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/opencart/system/');
+define('DIR_LANGUAGE', '/var/www/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/opencart/image/');
+define('DIR_CACHE', '/var/www/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/system/download/');
+define('DIR_UPLOAD', '/var/www/opencart/system/upload/');
+define('DIR_MODIFICATION', '/var/www/opencart/system/modification/');
+define('DIR_LOGS', '/var/www/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mtesitoo');
+define('DB_PREFIX', 'oc_');
