f5eec23c5564f97b1c64f35d776bdf44cd7dc0ab
David Edgar <david.r.edgar@gmail.com>
2015-08-25 12:43:43 +0100

    Fix config files for cloud server.

--- config-google_cloud.php
+++ config-google_cloud.php
@@ -1,27 +1,27 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://104.155.84.193/opencart/');
+define('HTTP_SERVER', 'http://104.155.84.193/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://104.155.84.193/opencart/');
+define('HTTPS_SERVER', 'https://104.155.84.193/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/opencart/catalog/');
-define('DIR_SYSTEM', '/var/www/opencart/system/');
-define('DIR_LANGUAGE', '/var/www/opencart/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/opencart/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/opencart/system/config/');
-define('DIR_IMAGE', '/var/www/opencart/image/');
-define('DIR_CACHE', '/var/www/opencart/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/opencart/system/download/');
-define('DIR_UPLOAD', '/var/www/opencart/system/upload/');
-define('DIR_MODIFICATION', '/var/www/opencart/system/modification/');
-define('DIR_LOGS', '/var/www/opencart/system/logs/');
+define('DIR_APPLICATION', '/var/www/catalog/');
+define('DIR_SYSTEM', '/var/www/system/');
+define('DIR_LANGUAGE', '/var/www/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/system/config/');
+define('DIR_IMAGE', '/var/www/image/');
+define('DIR_CACHE', '/var/www/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/system/download/');
+define('DIR_UPLOAD', '/var/www/system/upload/');
+define('DIR_MODIFICATION', '/var/www/system/modification/');
+define('DIR_LOGS', '/var/www/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'opencart');
+define('DB_USERNAME', 'mtesitoo');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'mtesitoo');
 define('DB_PREFIX', 'oc_');
