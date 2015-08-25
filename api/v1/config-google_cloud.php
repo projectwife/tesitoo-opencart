896d677bd55b1caba311be896de7f09bb0e60e8b
David Edgar <david.r.edgar@gmail.com>
2015-08-25 01:11:29 +0100

    Add config file for API for use on google cloud server.

---
+++ api/v1/config-google_cloud.php
@@ -0,0 +1,11 @@
+<?php
+
+if (file_exists('../../config.php')) {
+	require_once('../../config.php');
+}
+
+// DIR
+define('DIR_API_APPLICATION', '/var/www/api/v1/catalog/');
+define('DIR_API_SYSTEM', '/var/www/api/v1/system/');
+
+?>
