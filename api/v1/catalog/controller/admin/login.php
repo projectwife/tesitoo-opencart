2aa4df76fcee11a70b5537257d8e6e799e1272c7
ak-asif-khan <asif.cit08@gmail.com>
2015-09-05 18:35:01 -0400

    Login API for admin/vendor added. If username/password is correct the username, user_id and user_group_id is returned. user_group_id can be used to identify if vendor or site admin

---
+++ api/v1/catalog/controller/admin/login.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(DIR_API_APPLICATION . 'controller/admin/base/login_base.php');
+
+class ControllerAdminLoginAPI extends ControllerAdminLoginBaseAPI {
+
+	public function index($args = array()) {
+		parent::index($args);
+	}
+
+}
+
+?>
