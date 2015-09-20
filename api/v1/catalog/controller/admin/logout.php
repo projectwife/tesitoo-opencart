84d6d83543169d78c8437ade1573722eca5dafbc
ak-asif-khan <asif.cit08@gmail.com>
2015-09-20 18:32:12 -0400

    Add API for vendor logout

---
+++ api/v1/catalog/controller/admin/logout.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(DIR_API_APPLICATION . 'controller/admin/base/logout_base.php');
+
+class ControllerAdminLogoutAPI extends ControllerAdminLogoutBaseAPI {
+
+	public function index($args = array()) {
+		parent::index($args);
+	}
+
+}
+
+?>
