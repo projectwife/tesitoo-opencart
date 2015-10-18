06e1dd3027bf25e34747023bf5cfc29a3e6f8b74
David Edgar <david.r.edgar@gmail.com>
2015-10-18 10:15:45 +0100

    Initial work on delete product image.

--- api/v1/catalog/controller/product/product.php
+++ api/v1/catalog/controller/product/product.php
@@ -67,8 +67,13 @@ class ControllerProductProductAPI extends ControllerProductProductBaseAPI {
 	public function image($args = array()) {
 		$id = isset($args['id']) ? $args['id'] : null;
 
-		if($this->request->isPostRequest() && $id != null) {
+		var_dump($args);
+
+		if ($this->request->isPostRequest() && $id != null) {
 			$this->uploadImage($id);
+		}
+		else if ($this->request->isDeleteRequest() && $id != null) {
+			$this->deleteImage($id);
 		}
 		else {
 			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_METHOD_NOT_FOUND, ErrorCodes::getMessage(ErrorCodes::ERRORCODE_METHOD_NOT_FOUND));
@@ -201,6 +206,39 @@ class ControllerProductProductAPI extends ControllerProductProductBaseAPI {
 			$this->model_catalog_vdi_product
 							->addAuxProductImage($id, $sortOrder, $destination);
 		}
+	}
+
+	public function deleteImage($id) {
+		if ($this->user->isLogged()) {
+			$this->request->post['vendor'] = $this->user->getVP();
+		}
+		else {
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_UNAUTHORIZED, ErrorCodes::ERRORCODE_USER_NOT_LOGGED_IN, "not allowed");
+		}
+
+		$userName = $this->user->getUserName();
+		var_dump ($userName);
+
+		$imageFile = urldecode($this->request->get['file']);
+		var_dump ($imageFile);
+
+		//check this vendor owns specified product
+		$this->load->model('catalog/vdi_product');
+		$product = $this->model_catalog_vdi_product->getProduct((int)$id);
+
+		if ((!array_key_exists('vendor_id', $product)) ||
+			($this->user->getVP() != (int)($product['vendor_id']))) {
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_UNAUTHORIZED, ErrorCodes::ERRORCODE_VENDOR_NOT_ALLOWED, ErrorCodes::getMessage(ErrorCodes::ERRORCODE_VENDOR_NOT_ALLOWED));
+		}
+		//if $imageFile is main image, then set property to default ("" / null ?)
+
+		//else search for $imageFile in aux images
+		//if exists, delete
+
+		//remove file from image catalog
+
+		//do we need to do anything to clear up the image cache?
+
 	}
 
 	//ADDED: tesitoo - david - 2015-08-25 - override to add vendor id & name
