d29c8a321f0711abe074bc94b602b989848f653f
David Edgar <david.r.edgar@gmail.com>
2016-01-28 23:25:52 +0000

    Fix SMTP Host -> Hostname issue (https://github.com/opencart/opencart/issues/2887)

--- catalog/model/checkout/order.php
+++ catalog/model/checkout/order.php
@@ -676,7 +676,7 @@ class ModelCheckoutOrder extends Model {
 					$mail = new Mail();
 					$mail->protocol = $this->config->get('config_mail_protocol');
 					$mail->parameter = $this->config->get('config_mail_parameter');
-					$mail->smtp_hostname = $this->config->get('config_mail_smtp_host');
+					$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 					$mail->smtp_username = $this->config->get('config_mail_smtp_username');
 					$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
 					$mail->smtp_port = $this->config->get('config_mail_smtp_port');
@@ -770,7 +770,7 @@ class ModelCheckoutOrder extends Model {
 					$mail = new Mail();
 					$mail->protocol = $this->config->get('config_mail_protocol');
 					$mail->parameter = $this->config->get('config_mail_parameter');
-					$mail->smtp_hostname = $this->config->get('config_mail_smtp_host');
+					$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 					$mail->smtp_username = $this->config->get('config_mail_smtp_username');
 					$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
 					$mail->smtp_port = $this->config->get('config_mail_smtp_port');
@@ -830,7 +830,7 @@ class ModelCheckoutOrder extends Model {
 				$mail = new Mail();
 				$mail->protocol = $this->config->get('config_mail_protocol');
 				$mail->parameter = $this->config->get('config_mail_parameter');
-				$mail->smtp_hostname = $this->config->get('config_mail_smtp_host');
+				$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
 				$mail->smtp_username = $this->config->get('config_mail_smtp_username');
 				$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
 				$mail->smtp_port = $this->config->get('config_mail_smtp_port');
