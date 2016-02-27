476dcf018dccdab7eebb84a612cdad7c8a637d02
David Edgar <david.r.edgar@gmail.com>
2016-02-27 13:00:05 +0000

    Update notification texts.

--- admin/language/english/mail/email_notification.php
+++ admin/language/english/mail/email_notification.php
@@ -6,13 +6,13 @@ $_['heading_title']      = 'Add/Remove Product Email Notification';
 $_['text_to']    		= 'Dear %s,';
 $_['text_subject_add']  = 'New product %s submitted from %s';
 $_['text_subject_edit'] = '%s edited the product %s';
-$_['text_subject_approve'] = 'The %s Is Approved';
-$_['text_subject_expire'] = 'Your Account Will Expire In %s';
+$_['text_subject_approve'] = 'Your product has been approved: %s';
+$_['text_subject_expire'] = 'Your account will expire in %s';
 
-$_['text_message_add']  = 'This is to informed you that %s has submitted a new product <b>%s</b> to the store.';
-$_['text_message_edit'] = 'This is to informed you that product <b>%s</b> has been edited. Please review again !';
-$_['text_message_approve'] = 'This is to informed you that product <b>%s</b> has been approved.';
-$_['text_message_expire'] = 'This is to informed you that your account will expire in <b>%s</b>.<br/> Please renew the contract as soon as possible to avoid account closed.<br/><br/>Kindly login to your account, go to Personal Detail -> Contract History -> Renew Contract to renew the contract.';
+$_['text_message_add']  = 'This is to inform you that %s has submitted a new product <b>%s</b> to the store.';
+$_['text_message_edit'] = 'This is to inform you that product <b>%s</b> has been edited. Please review again!';
+$_['text_message_approve'] = 'This is to inform you that product <b>%s</b> has been approved.';
+$_['text_message_expire'] = 'This is to inform you that your account will expire in <b>%s</b>.<br/> Please renew the contract as soon as possible to avoid account closed.<br/><br/>Kindly login to your account, go to Personal Detail -> Contract History -> Renew Contract to renew the contract.';
 
 $_['text_thanks']    	= 'Thank You,';
 $_['text_system']    	= '<span class="help">This is system generated message. Please do not reply.</span>';
