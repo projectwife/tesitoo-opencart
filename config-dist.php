f372d96326e18fcd714bebf939adec7591bd3220
David Edgar <david.r.edgar@gmail.com>
2016-04-07 12:00:35 +0100

    Add contents for config-dist.php files. Update README.

---
+++ config-dist.php
@@ -0,0 +1,29 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/html/opencart/system/');
+define('DIR_LANGUAGE', '/var/www/html/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/html/opencart/image/');
+
+define('DIR_CACHE', '/var/www/html/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/opencart/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'tesitoo');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tesitoo');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
