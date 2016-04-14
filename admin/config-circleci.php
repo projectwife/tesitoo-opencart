f275d2444ec82ecaa5fa4f587b803a550d83d69f
David Edgar <david.r.edgar@gmail.com>
2016-04-14 19:22:16 +0100

    circleci-specific config files (for db)
    save screenshot and source of homepage for debugging purposes

---
+++ admin/config-circleci.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/opencart/admin/');
+define('DIR_SYSTEM', '/var/www/html/opencart/system/');
+define('DIR_LANGUAGE', '/var/www/html/opencart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/opencart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/html/opencart/image/');
+define('DIR_CACHE', '/var/www/html/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/opencart/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ubuntu');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'circle_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
