e99cd726a5d450dc771e4de447bdc835eea53601
David Edgar <david.r.edgar@gmail.com>
2016-05-07 02:04:21 +0100

    Scale and crop images to square, instead of scaling and leaving white postbox borders.

--- catalog/model/tool/image.php
+++ catalog/model/tool/image.php
@@ -40,4 +40,44 @@ class ModelToolImage extends Model {
 			return $this->config->get('config_url') . 'image/' . $new_image;
 		}
 	}
+	public function squareCropScale($filename, $squareSize) {
+		if (!is_file(DIR_IMAGE . $filename)) {
+			return;
+		}
+
+		$extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+		$old_image = $filename;
+		$new_image = 'cache/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . $squareSize . 'x' . $squareSize . '.' . $extension;
+
+		if (!is_file(DIR_IMAGE . $new_image) || (filectime(DIR_IMAGE . $old_image) > filectime(DIR_IMAGE . $new_image))) {
+			$path = '';
+
+			$directories = explode('/', dirname(str_replace('../', '', $new_image)));
+
+			foreach ($directories as $directory) {
+				$path = $path . '/' . $directory;
+
+				if (!is_dir(DIR_IMAGE . $path)) {
+					@mkdir(DIR_IMAGE . $path, 0777);
+				}
+			}
+
+			list($width_orig, $height_orig) = getimagesize(DIR_IMAGE . $old_image);
+
+			if ($width_orig != $squareSize || $height_orig != $squareSize) {
+				$image = new Image(DIR_IMAGE . $old_image);
+				$image->squareCropScale($squareSize);
+				$image->save(DIR_IMAGE . $new_image);
+			} else {
+				copy(DIR_IMAGE . $old_image, DIR_IMAGE . $new_image);
+			}
+		}
+
+		if ($this->request->server['HTTPS']) {
+			return $this->config->get('config_ssl') . 'image/' . $new_image;
+		} else {
+			return $this->config->get('config_url') . 'image/' . $new_image;
+		}
+	}
 }
