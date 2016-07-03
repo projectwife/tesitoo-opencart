bb7900670af944fcaac8bf7cce61acd0ae09e43a
David Edgar <david.r.edgar@gmail.com>
2016-07-04 00:23:49 +0100

    Check that country_id and zone_id are numeric in vendor registration request.
    Correct error codes.

--- api/v1/catalog/controller/vendor/register.php
+++ api/v1/catalog/controller/vendor/register.php
@@ -74,46 +74,46 @@ class ControllerVendorRegisterAPI extends ApiController {
 		$data = parent::getInternalRouteData('account/signup/index');
 
 		if ('' != ($data['error_warning'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_warning']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_warning']);
 		}
 		if ('' != ($data['error_username'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_username']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_username']);
 		}
 		if ('' != ($data['error_firstname'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_firstname']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_firstname']);
 		}
 		if ('' != ($data['error_lastname'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_lastname']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_lastname']);
 		}
 		if ('' != ($data['error_email'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_email']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_email']);
 		}
 		if ('' != ($data['error_paypal'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_paypal']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_paypal']);
 		}
 		if ('' != ($data['error_telephone'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_telephone']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_telephone']);
 		}
 		if ('' != ($data['error_password'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_password']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_password']);
 		}
 		if ('' != ($data['error_address_1'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_address_1']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_address_1']);
 		}
 		if ('' != ($data['error_city'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_city']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_city']);
 		}
 		if ('' != ($data['error_company'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_company']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_company']);
 		}
 		if ('' != ($data['error_postcode'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_postcode']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_postcode']);
 		}
 		if ('' != ($data['error_country'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_country']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_country']);
 		}
 		if ('' != ($data['error_zone'])) {
-			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, $data['error_zone']);
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_BAD_PARAMETER, $data['error_zone']);
 		}
 
 	}
