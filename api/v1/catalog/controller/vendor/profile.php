4b1be3fc96c7dee1a57f7a0bca6c75dffa549e60
David Edgar <david.r.edgar@gmail.com>
2016-07-31 01:21:08 +0100

    GET and POST vendor profiles.

--- api/v1/catalog/controller/vendor/profile.php
+++ api/v1/catalog/controller/vendor/profile.php
@@ -41,7 +41,7 @@ class ControllerVendorProfileAPI extends ApiController {
 			'thumb' => $this->model_tool_image->resize($vendor['vendor_image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height')),
 			'telephone' => $vendor['telephone'],
 			'company' => $vendor['company'],
-			'description' => $vendor['vendor_description'],
+			'vendor_description' => $vendor['vendor_description'],
 			'email' => $vendor['email'],
 			'firstname' => $vendor['firstname'],
 			'lastname' => $vendor['lastname'],
@@ -60,6 +60,60 @@ class ControllerVendorProfileAPI extends ApiController {
 
 	public function post() {
 
+        $this->load->model('catalog/vendor');
+		$vendor = $this->model_catalog_vendor->getVendor($this->user->getVP());
+
+		if(empty($vendor)) {
+			throw new ApiException(ApiResponse::HTTP_RESPONSE_CODE_NOT_FOUND, ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND, ErrorCodes::getMessage(ErrorCodes::ERRORCODE_VENDOR_NOT_FOUND));
+		}
+
+		if (isset($this->request->post['company'])) {
+			$vendor['company'] = $this->request->post['company'];
+            if (!isset($this->request->post['company'])) {
+                $vendor['vendor_name'] = $this->request->post['company'];
+            }
+		}
+        if (isset($this->request->post['vendor_name'])) {
+			$vendor['vendor_name'] = $this->request->post['vendor_name'];
+            if (!isset($this->request->post['company'])) {
+                $vendor['company'] = $this->request->post['vendor_name'];
+            }
+		}
+		if (isset($this->request->post['vendor_description'])) {
+			$vendor['vendor_description'] = $this->request->post['vendor_description'];
+		}
+		if (isset($this->request->post['telephone'])) {
+			$vendor['telephone'] = $this->request->post['telephone'];
+		}
+		if (isset($this->request->post['email'])) {
+			$vendor['email'] = $this->request->post['email'];
+		}
+		if (isset($this->request->post['firstname'])) {
+			$vendor['firstname'] = $this->request->post['firstname'];
+		}
+		if (isset($this->request->post['lastname'])) {
+			$vendor['lastname'] = $this->request->post['lastname'];
+		}
+		if (isset($this->request->post['address_1'])) {
+			$vendor['address_1'] = $this->request->post['address_1'];
+		}
+		if (isset($this->request->post['address_2'])) {
+			$vendor['address_2'] = $this->request->post['address_2'];
+		}
+		if (isset($this->request->post['city'])) {
+			$vendor['city'] = $this->request->post['city'];
+		}
+		if (isset($this->request->post['postcode'])) {
+			$vendor['postcode'] = $this->request->post['postcode'];
+		}
+		if (isset($this->request->post['country_id'])) {
+			$vendor['country_id'] = (int)$this->request->post['country_id'];
+		}
+		if (isset($this->request->post['zone_id'])) {
+			$vendor['zone_id'] = (int)$this->request->post['zone_id'];
+		}
+
+		$this->model_catalog_vendor->editVendor($vendor);
 	}
 
 }
