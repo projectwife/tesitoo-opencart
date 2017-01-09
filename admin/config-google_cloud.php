381274d523c5fa1b55848545052a305fcdce38a8
David Edgar <david.r.edgar@gmail.com>
2017-01-09 20:24:52 +0000

    Update config scripts for google cloud.

--- admin/config-google_cloud.php
+++ admin/config-google_cloud.php
@@ -1,31 +1,35 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://104.155.84.193/admin/');
-define('HTTP_CATALOG', 'http://104.155.84.193/');
+//define('HTTP_SERVER', 'http://104.199.17.125/admin/');
+//define('HTTP_CATALOG', 'http://104.199.17.125/');
+define('HTTP_SERVER', 'http://tesitoo.com/admin/');
+define('HTTP_CATALOG', 'http://tesitoo.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://104.155.84.193/admin/');
-define('HTTPS_CATALOG', 'https://104.155.84.193/');
+//define('HTTPS_SERVER', 'https://104.199.17.125/admin/');
+//define('HTTPS_CATALOG', 'https://104.199.17.125/');
+define('HTTPS_SERVER', 'https://tesitoo.com/admin/');
+define('HTTPS_CATALOG', 'https://tesitoo.com/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/admin/');
-define('DIR_SYSTEM', '/var/www/system/');
-define('DIR_LANGUAGE', '/var/www/admin/language/');
-define('DIR_TEMPLATE', '/var/www/admin/view/template/');
-define('DIR_CONFIG', '/var/www/system/config/');
-define('DIR_IMAGE', '/var/www/image/');
-define('DIR_CACHE', '/var/www/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/system/storage/download/');
-define('DIR_UPLOAD', '/var/www/system/storage/upload/');
-define('DIR_LOGS', '/var/www/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/system/storage/modification/');
-define('DIR_CATALOG', '/var/www/catalog/');
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/storage/download/');
+define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
+define('DIR_LOGS', '/var/www/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/storage/modification/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'mtesitoo');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'tesitoo');
+define('DB_PASSWORD', ''); //ASK DAVID
 define('DB_DATABASE', 'tesitoo');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
