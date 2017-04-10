79a5a440ad2e573edbbd84a01b93488dd9cac5e7
David Edgar <david.r.edgar@gmail.com>
2017-04-10 18:48:45 +0100

    commit before rebase

--- catalog/controller/common/vendor_terms.php
+++ catalog/controller/common/vendor_terms.php
@@ -3,9 +3,9 @@ class ControllerCommonVendorTerms extends Controller {
 	public function index() {
 		$this->load->model('information/information');
 
-		//$result = $this->model_information_information->getVendorTerms();
+		$result = $this->model_information_information->getVendorTerms();
 
-		//$data['vendor_terms'] = $result['vendor_terms'];
+		$data['vendor_terms'] = $result['vendor_terms'];
 
 		return $data;
 	}
