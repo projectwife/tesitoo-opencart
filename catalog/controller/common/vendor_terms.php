3a37d72cd8653d3568941c11e46c4d36b133b0c7
David Edgar <david.r.edgar@gmail.com>
2017-04-10 18:41:41 +0100

    Placeholder controller and model for returning vendor terms.

---
+++ catalog/controller/common/vendor_terms.php
@@ -0,0 +1,12 @@
+<?php
+class ControllerCommonVendorTerms extends Controller {
+	public function index() {
+		$this->load->model('information/information');
+
+		//$result = $this->model_information_information->getVendorTerms();
+
+		//$data['vendor_terms'] = $result['vendor_terms'];
+
+		return $data;
+	}
+}
