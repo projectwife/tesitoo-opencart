f225b3e975053b0af95effdb17b1354409c02168
David Edgar <david.r.edgar@gmail.com>
2017-10-07 21:02:11 +0100

    selenium missing import

--- test/registerVendor.py
+++ test/registerVendor.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
