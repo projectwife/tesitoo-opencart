7c7db2e444ad97d912aed99797d02d71624c72ae
David Edgar <david.r.edgar@gmail.com>
2017-10-08 12:40:43 +0100

    CircleCI v2.0 revert password changes; just install mysql client

--- admin/config-circleci.php
+++ admin/config-circleci.php
@@ -25,7 +25,7 @@ define('DIR_CATALOG', '/home/circleci/tesitoo-opencart/catalog/');
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', '127.0.0.1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'password');
 define('DB_DATABASE', 'circle_test');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
