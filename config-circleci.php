9a4fee55fbcd28dfb63f427c9bf35dd001c1b933
David Edgar <david.r.edgar@gmail.com>
2017-10-08 12:30:41 +0100

    CircleCI v2.0 Try MySQL with no password

--- config-circleci.php
+++ config-circleci.php
@@ -22,7 +22,7 @@ define('DIR_UPLOAD', '/home/circleci/tesitoo-opencart/system/storage/upload/');
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', '127.0.0.1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'circle_test');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
