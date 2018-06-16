74b754e5b4c05ee2aaf7ddef93d5dfff194910fc
David Edgar <david.r.edgar@gmail.com>
2018-06-16 20:58:07 +0100

    crop all images to square

--- admin/model/tool/vdi_image.php
+++ admin/model/tool/vdi_image.php
@@ -1,6 +1,7 @@
 <?php
 class ModelToolVDIImage extends Model {
 	public function resize($filename, $width, $height) {
+
 		if (!is_file(DIR_IMAGE . $filename)) {
 			return;
 		}
@@ -27,7 +28,7 @@ class ModelToolVDIImage extends Model {
 
 			if ($width_orig != $width || $height_orig != $height) {
 				$image = new Image(DIR_IMAGE . $old_image);
-				$image->resize($width, $height);
+				$image->squareCropScale($width, $height);
 				$image->save(DIR_IMAGE . $new_image);
 			} else {
 				copy(DIR_IMAGE . $old_image, DIR_IMAGE . $new_image);
--- catalog/model/tool/image.php
+++ catalog/model/tool/image.php
@@ -27,7 +27,7 @@ class ModelToolImage extends Model {
 
 			if ($width_orig != $width || $height_orig != $height) {
 				$image = new Image(DIR_IMAGE . $old_image);
-				$image->resize($width, $height);
+				$image->squareCropScale($width, $height);
 				$image->save(DIR_IMAGE . $new_image);
 			} else {
 				copy(DIR_IMAGE . $old_image, DIR_IMAGE . $new_image);
