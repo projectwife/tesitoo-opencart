caa2445d1c7906e4355f730f353c7ca36575ef67
David Edgar <david.r.edgar@gmail.com>
2018-09-25 00:21:38 +0100

    link to tesitoo_order from dashboard

--- admin/controller/dashboard/sale.php
+++ admin/controller/dashboard/sale.php
@@ -37,7 +37,7 @@ class ControllerDashboardSale extends Controller {
 			$data['total'] = round($sale_total);
 		}
 
-		$data['sale'] = $this->url->link('sale/order', 'token=' . $this->session->data['token'], 'SSL');
+		$data['sale'] = $this->url->link('sale/tesitoo_order', 'token=' . $this->session->data['token'], 'SSL');
 
 		return $this->load->view('dashboard/sale.tpl', $data);
 	}
